2fc95e1ab2f17ec8ce15fd949987014128d3fd6a
Ben Chambers <bchambers@clearfoundation.com>
2011-10-13 21:00:00 +0000

    - Started conversion on egress firewall app

---
+++ controllers/egress_firewall.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Egress firewall controller.
+ *
+ * @category   Apps
+ * @package    Egress_Firewall
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/egress_firewall/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Egress firewall controller.
+ *
+ * @category   Apps
+ * @package    Egress_Firewall
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/egress_firewall/
+ */
+
+class Egress_Firewall extends ClearOS_Controller
+{
+    /**
+     * Firewall server overview.
+     *
+     * @return view
+     */
+
+    function index()
+    {
+        // Load libraries
+        //---------------
+
+        $this->lang->load('egress_firewall');
+
+        // Load views
+        //-----------
+
+        $views = array('egress_firewall/port', 'egress_firewall/domain');
+
+        $this->page->view_forms($views, lang('egress_firewall_app_name'));
+    }
+}
