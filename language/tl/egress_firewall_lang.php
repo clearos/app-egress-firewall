b4885efa1e585e737bc54b4490c75d580ff8f798
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:57:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Payagan ang lahat';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Payagan ang lahat ng mga palabas na trapiko - tukuyin block destinasyon';
+$lang['egress_firewall_app_description'] = 'Pinapayagan ka ng labasan Firewall app mong i-block tiyak na mga uri ng trapiko mula sa umaalis sa iyong network.';
+$lang['egress_firewall_app_name'] = 'labasan Firewall';
+$lang['egress_firewall_block_all'] = 'block All';
+$lang['egress_firewall_destination_domains'] = 'Destinasyon Domains';
+$lang['egress_firewall_destination_ports'] = 'Destinasyon Port (s)';
+$lang['egress_firewall_mode'] = 'paraan';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Harangan ang lahat ng mga palabas na trapiko - tukuyin pinapayagan destinasyon';
