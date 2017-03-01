ed951dcca3ccada1fa0c1e14e1553c32f4bdebf4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:35:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'dozvoli sve';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Dozvoli sve odlazni saobraćaj - navesti blok odredišta';
+$lang['egress_firewall_app_description'] = 'Aplikacija Egress Firewall vam omogućava da blokiraju određene vrste prometa napusti svoju mrežu.';
+$lang['egress_firewall_app_name'] = 'izlazak Firewall';
+$lang['egress_firewall_block_all'] = 'blokirati sve';
+$lang['egress_firewall_destination_domains'] = 'Destinacija domena';
+$lang['egress_firewall_destination_ports'] = 'Odredište Port (e)';
+$lang['egress_firewall_mode'] = 'način';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokira sav odlazni saobraćaj - navesti dozvoljeno odredišta';
