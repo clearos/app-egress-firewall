91d8bdd3d92a7d04872909f78f6f29b6c2c7b834
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:01:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Engedélyezni az összeset';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Lehetővé teszi az összes kimenő forgalom - block célpontok';
+$lang['egress_firewall_app_description'] = 'A kiáramlás tűzfal alkalmazás lehetővé teszi, hogy blokkolja bizonyos típusú forgalmat elhagyják a hálózatot.';
+$lang['egress_firewall_app_name'] = 'egress tűzfal';
+$lang['egress_firewall_block_all'] = 'Összes letiltása';
+$lang['egress_firewall_destination_domains'] = 'Destination Domains';
+$lang['egress_firewall_destination_ports'] = 'Destination port (ok)';
+$lang['egress_firewall_mode'] = 'Mód';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Letiltja az összes kimenő forgalom - adja engedélyezve célpontok';
