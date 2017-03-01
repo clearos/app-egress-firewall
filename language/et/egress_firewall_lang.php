7b5bf928aba241d27aee42f29a7ddad30fbd4c3a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:48:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Luba kõik';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Laske kõik väljuva liikluse - täpsustada plokk sihtkohtadesse';
+$lang['egress_firewall_app_description'] = 'Gaaside väljapääsu tulemüüri rakendus võimaldab teil blokeerida teatud liiki liiklust jättes oma võrgustikku.';
+$lang['egress_firewall_app_name'] = 'Egress tulemüür';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Sihtkoht Domeenid';
+$lang['egress_firewall_destination_ports'] = 'Sihtsadamas (s)';
+$lang['egress_firewall_mode'] = 'viis';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokeeri kõik väljuva liikluse - määrata lubatud sihtkohtadesse';
