7f9e1a48f6cbda3ca07a6c8e4a347fd4ace0e240
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:58:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'caniatáu All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Caniatáu i&#39;r holl draffig sy&#39;n mynd allan - nodwch cyrchfannau bloc';
+$lang['egress_firewall_app_description'] = 'Mae&#39;r app Allanfa Firewall yn eich galluogi i rwystro rhai mathau o draffig rhag gadael eich rhwydwaith.';
+$lang['egress_firewall_app_name'] = 'Firewall allan';
+$lang['egress_firewall_block_all'] = 'bloc pob';
+$lang['egress_firewall_destination_domains'] = 'Parthau cyrchfan';
+$lang['egress_firewall_destination_ports'] = 'Cyrchfan Port (au)';
+$lang['egress_firewall_mode'] = 'modd';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloc holl draffig sy&#39;n mynd allan - nodwch cyrchfannau a ganiateir';
