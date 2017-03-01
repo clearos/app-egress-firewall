899fd83987c900a6191d008707793fff2c9738c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:32:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Tillat alle';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Tillat all utgående trafikk - angi blokk destinasjoner';
+$lang['egress_firewall_app_description'] = 'The Egress brannmur app lar deg blokkere visse typer trafikk fra å forlate nettverket ditt.';
+$lang['egress_firewall_app_name'] = 'egress brannmur';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'destinasjon domener';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Modus';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokkere all utgående trafikk - angi tillatt destinasjoner';
