ac930c2abefdd6694b4bd7307336c99711f58c84
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:29:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'bütün imkan';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'block istiqamətlərə müəyyən - bütün çıxan trafik imkan verir';
+$lang['egress_firewall_app_description'] = 'Egress Firewall app sizin şəbəkə tərk trafik müəyyən növ qarşısını almaq üçün imkan verir.';
+$lang['egress_firewall_app_name'] = 'Egress Firewall';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Destination Domains';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'bütün gedən yol Block - icazə istiqamətlərə daxil';
