7fc5676a539e9181bc856546f41bbb07e20c95f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:04:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Ceadaigh uile';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Ceadaigh gach tráchta ag dul as oifig - sonraigh scríbe bloc';
+$lang['egress_firewall_app_description'] = 'Ceadaíonn an app Dul Amach Firewall tú chun bealach cineálacha éagsúla tráchta ó fhágáil do líonra.';
+$lang['egress_firewall_app_name'] = 'Balla Dóiteáin fágála';
+$lang['egress_firewall_block_all'] = 'Bloc Gach';
+$lang['egress_firewall_destination_domains'] = 'Fearainn Scríbe';
+$lang['egress_firewall_destination_ports'] = 'Scríbe Port (s)';
+$lang['egress_firewall_mode'] = 'mód';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloc gach tráchta ag dul as oifig - sonraigh scríbe cheadaítear';
