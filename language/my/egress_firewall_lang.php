dba44140f469aa65057cff2e24eaaeea0460221f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:37:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'အားလုံး Allow';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'အားလုံးအထွက်အသွားအလာ Allow - ပိတ်ပင်တားဆီးမှုနေရာများသတ်မှတ်';
+$lang['egress_firewall_app_description'] = 'အဆိုပါအီးဂရက်စ Firewall ကို app ကိုသင်သည်သင်၏ကွန်ယက်ကိုထွက်ခွာမှအသွားအလာအချို့အမျိုးမျိုးပိတ်ဆို့ဖို့ခွင့်ပြုပါတယ်။';
+$lang['egress_firewall_app_name'] = 'အီးဂရက်စ Firewall ကို';
+$lang['egress_firewall_block_all'] = 'block အားလုံး';
+$lang['egress_firewall_destination_domains'] = 'destination ဒိုမိန်း';
+$lang['egress_firewall_destination_ports'] = 'destination ဆိပ်ကမ်း (s)';
+$lang['egress_firewall_mode'] = 'mode ကို';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'အားလုံးအထွက်အသွားအလာပိတ်ဆို့ - ခွင့်ပြုနေရာများသတ်မှတ်';
