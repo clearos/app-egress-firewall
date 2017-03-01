17c6721338465e435ffd5ccc6a164d7da7f8f1e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:53:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Alles erlauben';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Lassen Sie alle ausgehenden Datenverkehr - angeben Block Ziele';
+$lang['egress_firewall_app_description'] = 'Der Egress-Firewall App ermöglicht es Ihnen zu verlassen Ihr Netzwerk bestimmte Arten von Datenverkehr zu blockieren.';
+$lang['egress_firewall_app_name'] = 'Egress-Firewall';
+$lang['egress_firewall_block_all'] = 'Alles blockieren';
+$lang['egress_firewall_destination_domains'] = 'Destination Domains';
+$lang['egress_firewall_destination_ports'] = 'Ziel-Port (s)';
+$lang['egress_firewall_mode'] = 'Modus';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Block alle ausgehenden Datenverkehr - Geben Sie erlaubten Ziele';
