e4d913a4f99ad58e6fa1f0b41ad7c6fa5b6988d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:14:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'ಎಲ್ಲಾ ಅನುಮತಿಸಿ';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'ಎಲ್ಲಾ ಹೊರಹೋಗುವ ಸಂಚಾರ ಅವಕಾಶ - ಬ್ಲಾಕ್ ಸ್ಥಳಗಳಿಗೆ ಸೂಚಿಸಲು';
+$lang['egress_firewall_app_description'] = 'ಮೋಕ್ಷ ಫೈರ್ವಾಲ್ ಅಪ್ಲಿಕೇಶನ್ ನಿಮ್ಮ ನೆಟ್ವರ್ಕ್ ತೊರೆಯದಂತೆ ದಟ್ಟಣೆಯ ನಿಶ್ಚಿತ ರೀತಿಯ ನಿರ್ಬಂಧಿಸಲು ಅನುಮತಿಸುತ್ತದೆ.';
+$lang['egress_firewall_app_name'] = 'ಮೋಕ್ಷ ಫೈರ್ವಾಲ್';
+$lang['egress_firewall_block_all'] = 'ಎಲ್ಲಾ ನಿರ್ಬಂಧಿಸಿ';
+$lang['egress_firewall_destination_domains'] = 'ಗಮ್ಯಸ್ಥಾನ ಡೊಮೇನ್ಗಳ';
+$lang['egress_firewall_destination_ports'] = 'ಗಮ್ಯಸ್ಥಾನ ಸಂಪರ್ಕಸ್ಥಾನ (ಗಳು)';
+$lang['egress_firewall_mode'] = 'ಮೋಡ್';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'ಎಲ್ಲಾ ಹೊರಹೋಗುವ ಸಂಚಾರ ನಿರ್ಬಂಧಿಸಲು - ಅವಕಾಶ ಸ್ಥಳಗಳಿಗೆ ಸೂಚಿಸಲು';
