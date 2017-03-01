83db6a2cbb99d89a254f53cc609432d3ccc3d6d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:40:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Дозвољавају све';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Дозволите сав одлазни саобраћај - навести блок дестинације';
+$lang['egress_firewall_app_description'] = 'Егресс фиревалл апликација вам омогућава да блокирају одређене врсте саобраћаја од напуштања мрежу.';
+$lang['egress_firewall_app_name'] = 'излазак заштитни зид';
+$lang['egress_firewall_block_all'] = 'блокира све';
+$lang['egress_firewall_destination_domains'] = 'Дестинатион Домени';
+$lang['egress_firewall_destination_ports'] = 'Дестинација порт (и)';
+$lang['egress_firewall_mode'] = 'начин';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Блокирају сви одлазни саобраћај - навести дозвољене дестинације';
