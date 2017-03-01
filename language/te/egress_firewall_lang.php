aee7a487f5a502f3f7c39ac13e559acb12a3224c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:52:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'అన్ని అనుమతించు';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'బయటకు వెళ్లే ట్రాఫిక్ అనుమతించు - బ్లాక్ గమ్యస్థానాలకు పేర్కొనండి';
+$lang['egress_firewall_app_description'] = 'లోపలికి ఫైర్వాల్ అనువర్తనం మీరు మీ నెట్వర్క్ వదిలి నుండి ట్రాఫిక్ కొన్ని రకాల నిరోధించేందుకు అనుమతిస్తుంది.';
+$lang['egress_firewall_app_name'] = 'లోపలికి ఫైర్వాల్';
+$lang['egress_firewall_block_all'] = 'అన్ని బ్లాక్';
+$lang['egress_firewall_destination_domains'] = 'గమ్యం డొమైన్';
+$lang['egress_firewall_destination_ports'] = 'గమ్యం పోర్ట్ (లు)';
+$lang['egress_firewall_mode'] = 'మోడ్';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'బయటకు వెళ్లే ట్రాఫిక్ బ్లాక్ - అనుమతి గమ్యస్థానాలకు పేర్కొనండి';
