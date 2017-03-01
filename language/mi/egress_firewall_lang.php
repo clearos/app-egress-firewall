82451084f8344032d404ccc7c3d54bf93c91cf18
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:22:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'tukua katoa';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Tukua waka putaatu katoa - whakapūtā ūnga poraka';
+$lang['egress_firewall_app_description'] = 'Te taupānga putanga Pātūahi taea ki a koe te aukati i etahi momo o te waka i te mahue koutou whatunga.';
+$lang['egress_firewall_app_name'] = 'Pātūahi putanga';
+$lang['egress_firewall_block_all'] = 'Poraka katoa';
+$lang['egress_firewall_destination_domains'] = 'Ngā rohe ūnga';
+$lang['egress_firewall_destination_ports'] = 'Ūnga Port (s)';
+$lang['egress_firewall_mode'] = 'aratau';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Poraka waka putaatu katoa - whakapūtā ūnga whakaaetia';
