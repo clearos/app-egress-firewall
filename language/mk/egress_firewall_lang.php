217f28f26a3d4f5e7e06ec487727f01a07fa3797
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:23:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Дозволи им на сите';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Дозволи им на сите излезни сообраќај - наведете блок дестинации';
+$lang['egress_firewall_app_description'] = 'Апликацијата за излез ѕид ви овозможува да се блокираат одредени видови на сообраќај од напуштање на вашата мрежа.';
+$lang['egress_firewall_app_name'] = 'излез Firewall';
+$lang['egress_firewall_block_all'] = 'Блокирај ги сите';
+$lang['egress_firewall_destination_domains'] = 'код на домени';
+$lang['egress_firewall_destination_ports'] = 'Дестинација порта (и)';
+$lang['egress_firewall_mode'] = 'режим';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Ги блокирате сите појдовни сообраќај - наведете дозволено дестинации';
