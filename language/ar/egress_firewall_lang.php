e7a623e701f974af6a885ab392281013db4c9742
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:26:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'اسمح للكل';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'السماح لجميع حركة المرور المنتهية ولايته - تحديد الأماكن كتلة';
+$lang['egress_firewall_app_description'] = 'التطبيق الخروج جدار حماية يسمح لك لمنع أنواع معينة من حركة المرور من مغادرة الشبكة.';
+$lang['egress_firewall_app_name'] = 'جدار حماية الخروج';
+$lang['egress_firewall_block_all'] = 'احظر الجميع';
+$lang['egress_firewall_destination_domains'] = 'مجالات المقصد';
+$lang['egress_firewall_destination_ports'] = 'ميناء الوجهة (ق)';
+$lang['egress_firewall_mode'] = 'الوضع';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'منع حركة المرور المنتهية ولايته - تحديد الأماكن المسموح بها';
