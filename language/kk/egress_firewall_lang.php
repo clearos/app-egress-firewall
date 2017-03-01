1b53549feb1e878bb223010b834eea3ef03ca330
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:11:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Барлық рұқсат беру';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'барлық шығыс трафик рұқсат ету - блок бағыттар көрсетіңіз';
+$lang['egress_firewall_app_description'] = 'Шығу Firewall қолданба желіні қалдырып трафиктің белгілі бір түрлерін блоктауға мүмкіндік береді.';
+$lang['egress_firewall_app_name'] = 'шығу Firewall';
+$lang['egress_firewall_block_all'] = 'Блок Барлық';
+$lang['egress_firewall_destination_domains'] = 'Destination Домендер';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (лар)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'барлық шығыс трафигін блоктау - рұқсат бағыттар көрсетіңіз';
