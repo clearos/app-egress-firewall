29f9efd932e804884bc2a754e3e18812c497f73d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:58:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Bada All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Bada dukan mai fita traffic - saka block destinations';
+$lang['egress_firewall_app_description'] = 'The Egress Firewall app ka damar toshe wani iri traffic daga barin your cibiyar sadarwa.';
+$lang['egress_firewall_app_name'] = 'Egress Firewall';
+$lang['egress_firewall_block_all'] = 'block All';
+$lang['egress_firewall_destination_domains'] = 'manufa Domains';
+$lang['egress_firewall_destination_ports'] = 'Manufa Port (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Toshe duk mai fita traffic - saka yarda destinations';
