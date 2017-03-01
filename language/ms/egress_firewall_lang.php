fa09ecddb72c535dac74303121ae706a6f1b444d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:28:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Benarkan Semua';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Benarkan semua trafik keluar - menentukan destinasi blok';
+$lang['egress_firewall_app_description'] = 'Aplikasi Jalan keluar Firewall membolehkan anda untuk menyekat jenis lalu lintas tertentu daripada meninggalkan rangkaian anda.';
+$lang['egress_firewall_app_name'] = 'Firewall jalan keluar';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Domain Destinasi';
+$lang['egress_firewall_destination_ports'] = 'Port Destinasi (s)';
+$lang['egress_firewall_mode'] = 'mod';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Menyekat semua trafik keluar - menentukan destinasi dibenarkan';
