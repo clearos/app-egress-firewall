8c42c75b53bec63de9e882aebf56bd15fdfba18b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:15:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = '모두 허용';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = '블록 대상을 지정 - 나가는 모든 트래픽 허용';
+$lang['egress_firewall_app_description'] = '출구 방화벽 응용 프로그램은 네트워크를 떠나 트래픽의 특정 종류를 차단할 수 있습니다.';
+$lang['egress_firewall_app_name'] = '출구 방화벽';
+$lang['egress_firewall_block_all'] = '모두 차단';
+$lang['egress_firewall_destination_domains'] = '대상 도메인';
+$lang['egress_firewall_destination_ports'] = '대상 포트 (들)';
+$lang['egress_firewall_mode'] = '방법';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = '나가는 모든 트래픽을 차단 - 허용 대상을 지정';
