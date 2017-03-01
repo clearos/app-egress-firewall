774a977c1e45efcab0f7de399e4197917dc536f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:48:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'ngidinan Sadaya';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Ngidinan sadaya lalu lintas kaluar - nangtukeun maksud block';
+$lang['egress_firewall_app_description'] = 'Nu Egress firewall app ngidinan Anjeun pikeun meungpeuk rupa tangtu lalu lintas ti ninggalkeun jaringan Anjeun.';
+$lang['egress_firewall_app_name'] = 'firewall Egress';
+$lang['egress_firewall_block_all'] = 'blok Sadaya';
+$lang['egress_firewall_destination_domains'] = 'domain tujuan';
+$lang['egress_firewall_destination_ports'] = 'Tujuan Port (s)';
+$lang['egress_firewall_mode'] = 'mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Meungpeuk sadaya lalu lintas kaluar - nangtukeun maksud diidinan';
