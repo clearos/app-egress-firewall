665ce694a8dc0e5e363fe8d3f3d73707a8d4c2f2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:56:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'બધા માટે પરવાનગી આપે છે';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'બહાર જતાં તમામ ટ્રાફિકને મંજૂરી - બ્લોક સ્થળો સ્પષ્ટ';
+$lang['egress_firewall_app_description'] = 'નિર્ગમન ફાયરવોલ એપ્લિકેશન તમે તમારા નેટવર્ક છોડીને ટ્રાફિક ચોક્કસ પ્રકારના અવરોધિત કરવા માટે પરવાનગી આપે છે.';
+$lang['egress_firewall_app_name'] = 'બહાર નીકળવાનો માર્ગ ફાયરવોલ';
+$lang['egress_firewall_block_all'] = 'તમામને અવરોધિત કરો';
+$lang['egress_firewall_destination_domains'] = 'લક્ષ્યસ્થાન ડોમેન્સ';
+$lang['egress_firewall_destination_ports'] = 'લક્ષ્યસ્થાન પોર્ટ (ઓ)';
+$lang['egress_firewall_mode'] = 'સ્થિતિ';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'બહાર જતાં તમામ ટ્રાફિક બ્લોક - મંજૂરી સ્થળો સ્પષ્ટ';
