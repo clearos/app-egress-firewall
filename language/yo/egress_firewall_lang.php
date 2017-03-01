3b31c52efd089dd98ce26e482719e5c0731efc57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:59:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'gba Gbogbo';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Gba gbogbo awọn ipe ti njade ijabọ - pato Àkọsílẹ ibi';
+$lang['egress_firewall_app_description'] = 'The Egress Firewall app faye gba o lati dènà awọn iru ti ijabọ lati nto kuro nẹtiwọki rẹ.';
+$lang['egress_firewall_app_name'] = 'Egress Ogiriina';
+$lang['egress_firewall_block_all'] = 'block Gbogbo';
+$lang['egress_firewall_destination_domains'] = 'nlo ibugbe';
+$lang['egress_firewall_destination_ports'] = 'Nlo Port (s)';
+$lang['egress_firewall_mode'] = 'mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Dènà gbogbo awọn ijabọ - pato laaye ibi';
