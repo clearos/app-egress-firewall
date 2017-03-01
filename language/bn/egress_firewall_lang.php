562dd18fc65f399483b4f888b1a932c83c50691d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:33:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'সকলের অনুমদিত';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'সব বহির্গামী ট্রাফিকের অনুমতি - ব্লক গন্তব্যস্থল উল্লেখ';
+$lang['egress_firewall_app_description'] = 'বাহিরে যাইবার পথ ফায়ারওয়াল অ্যাপ্লিকেশন আপনি আপনার নেটওয়ার্ক ত্যাগ থেকে ট্রাফিক নির্দিষ্ট ধরণের ব্লক করতে পারবেন.';
+$lang['egress_firewall_app_name'] = 'বহির্গমন ফায়ারওয়াল';
+$lang['egress_firewall_block_all'] = 'সব বন্ধ';
+$lang['egress_firewall_destination_domains'] = 'গন্তব্য ডোমেইন';
+$lang['egress_firewall_destination_ports'] = 'গন্তব্য পোর্ট (গুলি)';
+$lang['egress_firewall_mode'] = 'মোড';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'সব বিদায়ী যানজট - অনুমতি গন্তব্যস্থল উল্লেখ';
