73957f6128c54d5ce3fda5346830cfdeec0bd98e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:38:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'permiteţi toate';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Permite tot traficul de ieșire - specifică destinațiile bloc';
+$lang['egress_firewall_app_description'] = 'Aplicația Egress Firewall vă permite să blocați anumite tipuri de trafic de la părăsirea rețelei.';
+$lang['egress_firewall_app_name'] = 'Firewall egress';
+$lang['egress_firewall_block_all'] = 'Toate bloc';
+$lang['egress_firewall_destination_domains'] = 'Domenii de destinație';
+$lang['egress_firewall_destination_ports'] = 'Portul de destinație (e)';
+$lang['egress_firewall_mode'] = 'mod';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloca tot traficul de ieșire - specifică destinațiile permise';
