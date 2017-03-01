4a8a5b0bc03ae13de5d23822bdff1e7beffc0005
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:02:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Perbolehkan semua';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Mengizinkan semua lalu lintas keluar - tentukan tujuan block';
+$lang['egress_firewall_app_description'] = 'The Egress Firewall aplikasi memungkinkan Anda untuk memblokir beberapa jenis lalu lintas dari meninggalkan jaringan Anda.';
+$lang['egress_firewall_app_name'] = 'Firewall egress';
+$lang['egress_firewall_block_all'] = 'Memblokir semua';
+$lang['egress_firewall_destination_domains'] = 'Domain tujuan';
+$lang['egress_firewall_destination_ports'] = 'Tujuan Pelabuhan (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Memblokir semua lalu lintas keluar - tentukan tujuan diperbolehkan';
