fbc7b149e73a4ff68b34561d0a11d5af3680cf42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:17:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'ອະນຸຍາດໃຫ້ທັງຫມົດ';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'ອະນຸຍາດໃຫ້ການຈະລາຈອນລາຍຈ່າຍທັງຫມົດ - ລະບຸຈຸດຫມາຍປາຍທາງຕັນ';
+$lang['egress_firewall_app_description'] = 'ການ app Egress Firewall ອະນຸຍາດໃຫ້ທ່ານເພື່ອປ້ອງກັນການປະເພດໃດຫນຶ່ງຂອງການຈະລາຈອນອອກຈາກເຄືອຂ່າຍຂອງທ່ານ.';
+$lang['egress_firewall_app_name'] = 'Firewall Egress';
+$lang['egress_firewall_block_all'] = 'Block ທັງຫມົດ';
+$lang['egress_firewall_destination_domains'] = 'Domains ຈຸດຫມາຍປາຍທາງ';
+$lang['egress_firewall_destination_ports'] = 'ຈຸດຫມາຍປາຍທາງ Port (s)';
+$lang['egress_firewall_mode'] = 'ຮູບແບບການ';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'ປ້ອງກັນການຈະລາຈອນລາຍຈ່າຍທັງຫມົດ - ການລະບຸສະຖານທີ່ອະນຸຍາດໃຫ້';
