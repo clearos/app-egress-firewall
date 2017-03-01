280dbbb2affaff2c00164048d4dccee0b65afdc6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:57:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'pèmèt tout';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Pèmèt tout trafik sortan - presize destinasyon blòk';
+$lang['egress_firewall_app_description'] = 'App a sòti Firewall pèmèt ou bloke sèten kalite trafik nan kite rezo w lan.';
+$lang['egress_firewall_app_name'] = 'sòti Firewall';
+$lang['egress_firewall_block_all'] = 'Blòk tout';
+$lang['egress_firewall_destination_domains'] = 'destinasyon Domèn';
+$lang['egress_firewall_destination_ports'] = 'Destinasyon Port (yo)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloke tout trafik sortan - presize pèmèt destinasyon';
