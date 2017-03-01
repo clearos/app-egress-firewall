5bf45d8b617a1aede6031ebcefe381d5522945c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:55:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Επιτρέπονται όλα';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Επιτρέπουν όλη την εξερχόμενη κίνηση - καθορίσετε προορισμούς μπλοκ';
+$lang['egress_firewall_app_description'] = 'Η εφαρμογή εκκένωσης Τείχος προστασίας σας επιτρέπει να μπλοκάρουν ορισμένα είδη κυκλοφορίας φύγουν από το δίκτυό σας.';
+$lang['egress_firewall_app_name'] = 'έξοδο Τείχος προστασίας';
+$lang['egress_firewall_block_all'] = 'Αποκλεισμός όλων των';
+$lang['egress_firewall_destination_domains'] = 'Domains προορισμός';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Τρόπος';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Αποκλεισμός όλη την εξερχόμενη κίνηση - καθορίσετε επιτρέπεται προορισμοί';
