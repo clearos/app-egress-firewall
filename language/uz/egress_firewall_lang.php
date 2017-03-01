913c779e18a510967fe084d14286c78968c8e579
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:58:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Allow All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'blok maqsadlarni aniqlash - barcha chiquvchi trafik ruxsat berish';
+$lang['egress_firewall_app_description'] = 'Egress xavfsizlik devori ilova siz tarmoq tark trafik ayrim turdagi blokirovka qilish imkonini beradi.';
+$lang['egress_firewall_app_name'] = 'chiqish Firewall';
+$lang['egress_firewall_block_all'] = 'blok barcha';
+$lang['egress_firewall_destination_domains'] = 'Destination Maydonlar';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (lar)';
+$lang['egress_firewall_mode'] = 'usul';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Barcha chiquvchi trafik blokirovka - ruxsat maqsadlarni aniqlash';
