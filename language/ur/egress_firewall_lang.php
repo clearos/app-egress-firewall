73a367d2dd0a6f1a4c47297de0acb74919e65fa3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:58:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'تمام اجازت دیں';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'تمام سبکدوش ہونے والے ٹریفک کی اجازت دیں - بلاک منزلوں کی وضاحت';
+$lang['egress_firewall_app_description'] = 'Egress فصیل اپلی کیشن آپ کو آپ کے نیٹ ورک کے جانے سے ٹریفک کے بعض اقسام کو بلاک کرنے کی اجازت دیتا ہے.';
+$lang['egress_firewall_app_name'] = 'Egress فصیل';
+$lang['egress_firewall_block_all'] = 'بلاک تمام';
+$lang['egress_firewall_destination_domains'] = 'ہدف ڈومین';
+$lang['egress_firewall_destination_ports'] = 'منزل پورٹ (ے)';
+$lang['egress_firewall_mode'] = 'موڈ';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'تمام سبکدوش ہونے والے ٹریفک بلاک - اجازت دی منزلوں کی وضاحت';
