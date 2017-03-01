7a39c03eecf6931dec378219ca6e08dded9cca5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:43:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Dovoli vse';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Dovoli ves odhodni promet - določiti blok destinacij';
+$lang['egress_firewall_app_description'] = 'Aplikacija evakuacije požarni zid vam omogoča, da blokira določene vrste prometa, da zapustijo svoje omrežje.';
+$lang['egress_firewall_app_name'] = 'izstop Firewall';
+$lang['egress_firewall_block_all'] = 'Block Vse';
+$lang['egress_firewall_destination_domains'] = 'Ciljni domene';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'način';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokirati ves odhodni promet - določitev dovoljene destinacij';
