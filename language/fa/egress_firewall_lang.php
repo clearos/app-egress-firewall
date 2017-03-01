028f6169961230c7d963f1deb9542b82cd8f9ef8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:35:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'اجازه می دهد همه';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'اجازه می دهد همه ترافیک خروجی - مشخص مقصدهای بلوک';
+$lang['egress_firewall_app_description'] = 'برنامه خروجی فایروال اجازه می دهد تا به شما برای جلوگیری از انواع خاصی از ترافیک را از ترک شبکه خود را.';
+$lang['egress_firewall_app_name'] = 'فایروال خارج شدن';
+$lang['egress_firewall_block_all'] = 'مسدود کردن تمام';
+$lang['egress_firewall_destination_domains'] = 'دامنه مقصد';
+$lang['egress_firewall_destination_ports'] = 'بندر مقصد (بازدید کنندگان)';
+$lang['egress_firewall_mode'] = 'حالت';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'مسدود کردن تمام ترافیک خروجی - مشخص مقصدهای اجازه';
