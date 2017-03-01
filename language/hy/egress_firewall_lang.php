f55ec9ba577d6268ea2f70018f644fa06b189ee3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:28:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Թույլ տալ, որ բոլորը';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Թույլ տալ, որ բոլոր ելքային թրաֆիկ - նշել block ուղղություններով';
+$lang['egress_firewall_app_description'] = 'The ելք Firewall ծրագիրը թույլ է տալիս Ձեզ արգելափակել որոշակի տեսակի երթեւեկության թողնելով ձեր ցանցը.';
+$lang['egress_firewall_app_name'] = 'ելք Firewall';
+$lang['egress_firewall_block_all'] = 'Արգելափակել Բոլոր';
+$lang['egress_firewall_destination_domains'] = 'վերջնակետ Domains';
+$lang['egress_firewall_destination_ports'] = 'Նպատակակետ Port (ներ)';
+$lang['egress_firewall_mode'] = 'ռեժիմ';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Արգելափակել բոլոր ելքային թրաֆիկ - նշել թույլատրելի ուղղություններով';
