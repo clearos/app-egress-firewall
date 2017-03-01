c9c0c28c9622ff472488004aeee1241ee0a7ab22
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:12:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'អនុញ្ញាតឱ្យទាំងអស់';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'អនុញ្ញាតឱ្យចរាចរណ៍ចេញទាំងអស់ - បញ្ជាក់ទិសដៅប្លុក';
+$lang['egress_firewall_app_description'] = 'នេះ Egress ជញ្ជាំងភ្លើងកម្មវិធីអនុញ្ញាតឱ្យអ្នកដើម្បីរារាំងប្រភេទជាក់លាក់នៃចរាចរពីការចាកចេញពីបណ្តាញរបស់អ្នក។';
+$lang['egress_firewall_app_name'] = 'ជញ្ជាំងភ្លើង Egress';
+$lang['egress_firewall_block_all'] = 'ប្លុកទាំងអស់';
+$lang['egress_firewall_destination_domains'] = 'ដែនគោលដៅ';
+$lang['egress_firewall_destination_ports'] = 'គោលដៅច្រកទេ (s បាន)';
+$lang['egress_firewall_mode'] = 'របៀប';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'រារាំងចរាចរណ៍ចេញទាំងអស់ - បញ្ជាក់ទិសដៅដែលបានអនុញ្ញាត';
