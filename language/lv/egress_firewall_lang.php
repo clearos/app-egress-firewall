4181fe3500770e31cd4b180cab03e903f2ca0406
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:20:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'atļaut All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Atļaut visu izejošo trafiku - norādiet bloku galamērķus';
+$lang['egress_firewall_app_description'] = 'Izeja Firewall app ļauj bloķēt noteikta veida satiksmes atstāt savu tīklu.';
+$lang['egress_firewall_app_name'] = 'izeja Firewall';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Galamērķis Domains';
+$lang['egress_firewall_destination_ports'] = 'Galamērķis Port (s)';
+$lang['egress_firewall_mode'] = 'mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloķēt visu izejošo trafiku - norādiet atļauta galamērķus';
