2429daf9a538502b0ddf4806c5cfa20262d616b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:41:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'සියලු ඉඩ දෙන්න';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'සියලු පිටතට යන රථවාහන ඉඩ - වාරණ ගමනාන්ත නියම';
+$lang['egress_firewall_app_description'] = 'මෙම Egress ගිනි පවුර යෙදුම ඔබ ඔබේ ජාල පිටත් ගමනාගමනය ඇතැම් වර්ගවල අවහිර කිරීමට ඉඩ දෙයි.';
+$lang['egress_firewall_app_name'] = 'Egress ගිනි පවුර';
+$lang['egress_firewall_block_all'] = 'වාරණ සියලු';
+$lang['egress_firewall_destination_domains'] = 'ගමනාන්තය වසම්';
+$lang['egress_firewall_destination_ports'] = 'ගමනාන්තය වරාය (ව)';
+$lang['egress_firewall_mode'] = 'මාදිලිය';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'සියලු පිටතට යන රථවාහන අවහිර - අවසර ගමනාන්ත නියම';
