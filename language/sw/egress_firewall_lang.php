2eeee551ec7fac93dd1d05c2050c7a50fe8a2c06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:49:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'kuruhusu All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Kuruhusu trafiki wote anayemaliza muda wake - bayana kuzuia kivutio';
+$lang['egress_firewall_app_description'] = 'programu Egress Firewall utapata kuzuia aina fulani ya trafiki kutoka kuondoka mtandao wako.';
+$lang['egress_firewall_app_name'] = 'egress Firewall';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'marudio Domains';
+$lang['egress_firewall_destination_ports'] = 'Marudio Port (s)';
+$lang['egress_firewall_mode'] = 'mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Kuzuia trafiki zote zinazotoka - bayana kivutio kuruhusiwa';
