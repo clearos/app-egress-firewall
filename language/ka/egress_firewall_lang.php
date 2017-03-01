dc1a809fc443fee8d9eb74b5b6f1afd59ac5216d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:10:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'ნება ყველა';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'ნება ყველა გამავალი ტრაფიკი - დააკონკრეტა ბლოკი მიმართულებით';
+$lang['egress_firewall_app_description'] = 'გასასვლელი Firewall აპლიკაცია საშუალებას გაძლევთ ბლოკირება გარკვეული სახის ავტოტრანსპორტის ტოვებს თქვენს ქსელში.';
+$lang['egress_firewall_app_name'] = 'egress Firewall';
+$lang['egress_firewall_block_all'] = 'დაბლოკვა';
+$lang['egress_firewall_destination_domains'] = 'დანიშნულების Domains';
+$lang['egress_firewall_destination_ports'] = 'დანიშნულების პორტი (s)';
+$lang['egress_firewall_mode'] = 'რეჟიმი';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'დაბლოკოს ყველა გამავალი ტრაფიკი - დააკონკრეტა დაშვებული მიმართულებით';
