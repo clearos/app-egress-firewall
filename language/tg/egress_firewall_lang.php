d94850c413ad5c376f44982ecf243ebafa5ba5cb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:53:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Иҷоза ба ҳамаи';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Иҷоза ба ҳамаи ҳаракати содиротӣ - муайян самтҳо блоки';
+$lang['egress_firewall_app_description'] = 'Дар барнома Egress Сипар ба шумо имкон медиҳад маҳкам намудҳои муайяни воситаҳои нақлиёт аз тарк кардани шабакаи шумо.';
+$lang['egress_firewall_app_name'] = 'Сипар Egress';
+$lang['egress_firewall_block_all'] = 'Блоки Ҳамаи';
+$lang['egress_firewall_destination_domains'] = 'Доирањои Самт';
+$lang['egress_firewall_destination_ports'] = 'Самт Порт (р)';
+$lang['egress_firewall_mode'] = 'ҳолати';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Маҳкам намудани тамоми ҳаракати содиротӣ - муайян самтҳо иҷозат дода';
