a1a4b59a8bee4946a2f2eb966dff927f31865465
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:27:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'सर्व परवानगी द्या';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'सर्व जाणारी रहदारी द्या - ब्लॉक गंतव्ये निर्देशीत';
+$lang['egress_firewall_app_description'] = 'बाहेर जाण्याचा मार्ग फायरवॉल अनुप्रयोग आपण आपल्या नेटवर्क सोडण्यापासून वाहतूक विशिष्ट प्रकारच्या अवरोधित करण्याची अनुमती देते.';
+$lang['egress_firewall_app_name'] = 'बाहेर जाण्याचा मार्ग फायरवॉल';
+$lang['egress_firewall_block_all'] = 'सर्व अवरोधित करा';
+$lang['egress_firewall_destination_domains'] = 'गंतव्य डोमेन';
+$lang['egress_firewall_destination_ports'] = 'गंतव्य पोर्ट (चे)';
+$lang['egress_firewall_mode'] = 'मोड';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'सर्व जाणारी रहदारी अवरोधित करा - परवानगी गंतव्ये निर्देशीत';
