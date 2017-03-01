c88d04e91a70fa45de6665cb3d271c39515a7601
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:31:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Sta alles toe';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Laat al het uitgaande verkeer - specificeren blok bestemmingen';
+$lang['egress_firewall_app_description'] = 'De Egress Firewall app kunt u bepaalde soorten verkeer te blokkeren van het verlaten van uw netwerk.';
+$lang['egress_firewall_app_name'] = 'egress Firewall';
+$lang['egress_firewall_block_all'] = 'Blokkeer alles';
+$lang['egress_firewall_destination_domains'] = 'bestemming Domeinen';
+$lang['egress_firewall_destination_ports'] = 'Bestemming Port (s)';
+$lang['egress_firewall_mode'] = 'mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokkeer al het uitgaande verkeer - specificeren toegestaan ​​bestemmingen';
