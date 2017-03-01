af1e7dcb6b50309628eda6a60d040abafe626c88
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:30:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'सबै अनुमति';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'ब्लक गन्तव्यहरू निर्दिष्ट - सबै बाहिर जाने यातायात अनुमति';
+$lang['egress_firewall_app_description'] = 'यो Egress फायरवल अनुप्रयोग तपाईं आफ्नो नेटवर्क छोडेर देखि यातायात को केहि प्रकार ब्लक गर्न अनुमति दिन्छ।';
+$lang['egress_firewall_app_name'] = 'Egress फायरवल';
+$lang['egress_firewall_block_all'] = 'सबै ब्लक';
+$lang['egress_firewall_destination_domains'] = 'गन्तव्य डोमेन';
+$lang['egress_firewall_destination_ports'] = 'गन्तव्य पोर्ट (हरू)';
+$lang['egress_firewall_mode'] = 'मोड';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'सबै बाहिर जाने यातायात ब्लक - अनुमति गन्तव्यहरू निर्दिष्ट';
