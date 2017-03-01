50128517e664d270ec023a89ed77d41de9add930
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:47:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'permesi Ĉiuj';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Permesigi ĉiujn eksiĝinta trafiko - specifi bloko destinoj';
+$lang['egress_firewall_app_description'] = 'La eliro Firewall app permesas vin bloki iujn specojn de trafiko de lasanta vian reton.';
+$lang['egress_firewall_app_name'] = 'eliro Firewall';
+$lang['egress_firewall_block_all'] = 'bloko Ĉiuj';
+$lang['egress_firewall_destination_domains'] = 'destino Domajnoj';
+$lang['egress_firewall_destination_ports'] = 'Destino Haveno (j)';
+$lang['egress_firewall_mode'] = 'mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloki ĉiuj eksiĝinta trafiko - specifi permesis destinoj';
