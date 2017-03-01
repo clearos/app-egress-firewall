1386519b208062b4c6ca9fbf7bef40c82d5c1447
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:59:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Vumela Konke';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Vumela konke okuhamba Eziphumayo - ucacise block izindawo';
+$lang['egress_firewall_app_description'] = 'Insiza Yokuphuma firewall ikuvumela ukuba ukuvimba izinhlobo ezithile traffic kusuka kokushiya kunethiwekhi yakho.';
+$lang['egress_firewall_app_name'] = 'firewall ukuphuma';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Domains Destination';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Vimba zonke izimoto eziphumayo - ucacise izindawo bavunyelwe';
