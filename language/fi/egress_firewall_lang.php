cfd01427934f9018c5b975c74d1989be720cc5b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:49:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Salli kaikki';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Salli kaikki lähtevä liikenne - täsmentää block kohteet';
+$lang['egress_firewall_app_description'] = 'Egress Firewall app voit estää tietynlaiset liikennettä poistumasta verkkoon.';
+$lang['egress_firewall_app_name'] = 'egress Firewall';
+$lang['egress_firewall_block_all'] = 'Estä kaikki';
+$lang['egress_firewall_destination_domains'] = 'Kohde Verkkotunnukset';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (t)';
+$lang['egress_firewall_mode'] = 'tila';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Estää kaiken lähtevän liikenteen - määritä sallittu kohteet';
