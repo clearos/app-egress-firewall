f357362d19c0580a3b11c1bcdcd92ccf79ac138c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:45:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'U ogolow All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'U oggolow dhamaan gaadiidka xilka - sheeg dalal block';
+$lang['egress_firewall_app_description'] = 'The app baxo Brannmur kuu ogolaanaya in aad xannibi qaarkood gaadiidka ka tago aad network.';
+$lang['egress_firewall_app_name'] = 'Brannmur baxo';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Domains Destination';
+$lang['egress_firewall_destination_ports'] = 'Port (s) Destination';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Jooji dhamaan gaadiidka xilka - sheeg dalal ogol yahay';
