c1b15d10485dfaf3193e941f316a19e8476a5da9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:58:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'дозволити всі';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Дозволити весь вихідний трафік - вказати блок призначення';
+$lang['egress_firewall_app_description'] = 'Додаток Виходу Firewall дозволяє блокувати певні види трафіку з вхідних в локальну мережу.';
+$lang['egress_firewall_app_name'] = 'виходу Firewall';
+$lang['egress_firewall_block_all'] = 'блокувати всі';
+$lang['egress_firewall_destination_domains'] = 'напрямок Домени';
+$lang['egress_firewall_destination_ports'] = 'Порт призначення (и)';
+$lang['egress_firewall_mode'] = 'режим';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Блокувати весь вихідний трафік - вказати дозволені пункти призначення';
