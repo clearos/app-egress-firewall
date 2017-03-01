d55c22d7816d44f5f65d583240eaa8a8647b6275
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:30:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Onartu guztiak';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Baimendu irteerako trafikoa guztiak - Zehaztu bloke helmuga';
+$lang['egress_firewall_app_description'] = 'Egress Firewall aplikazioarekin, trafiko mota batzuk blokeatu ditzakezu zure sare utziz.';
+$lang['egress_firewall_app_name'] = 'egress Firewall';
+$lang['egress_firewall_block_all'] = 'Bloke guztiak';
+$lang['egress_firewall_destination_domains'] = 'Helmuga domeinuak';
+$lang['egress_firewall_destination_ports'] = 'Helmuga Port (k)';
+$lang['egress_firewall_mode'] = 'Modu';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokeatu irteerako trafikoa guztiak - Zehaztu onartzen helmuga';
