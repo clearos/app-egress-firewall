b9dedce16402539be088c08a801528a8853095d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:58:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Chấp nhận tất cả';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Cho phép tất cả lưu lượng gửi đi - chỉ định đích khối';
+$lang['egress_firewall_app_description'] = 'Các ứng dụng Egress Firewall cho phép bạn chặn một số loại lưu lượng từ rời mạng của bạn.';
+$lang['egress_firewall_app_name'] = 'Firewall Egress';
+$lang['egress_firewall_block_all'] = 'Chặn tất cả';
+$lang['egress_firewall_destination_domains'] = 'Tên miền điểm đến';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Chế độ';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Chặn tất cả lưu lượng gửi đi - chỉ định đích cho phép';
