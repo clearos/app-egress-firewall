e023990376d3fedf8080e52a5094a92c17d06dca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:41:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = '允许全部';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = '允许所有传出流量 - 指定块目的地';
+$lang['egress_firewall_app_description'] = '出口防火墙应用程序允许您离开您的网络阻止某些类型的流量。';
+$lang['egress_firewall_app_name'] = '出口防火墙';
+$lang['egress_firewall_block_all'] = '全部阻止';
+$lang['egress_firewall_destination_domains'] = '目标域';
+$lang['egress_firewall_destination_ports'] = '目的端口（S）';
+$lang['egress_firewall_mode'] = '模式';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = '阻止所有出站流量 - 指定允许目的地';
