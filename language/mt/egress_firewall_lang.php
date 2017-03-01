9812343b552dce62ee2847389d3619a1b9c8addb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:29:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Ħalli kollha';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Ħalli traffiku kollu ħierġa - tispeċifika destinazzjonijiet blokk';
+$lang['egress_firewall_app_description'] = 'Il-app egress Firewall jippermettilek li jimblokka ċerti tipi ta &#39;traffiku milli jitilqu network tiegħek.';
+$lang['egress_firewall_app_name'] = 'Firewall ħruġ';
+$lang['egress_firewall_block_all'] = 'blokk kollha';
+$lang['egress_firewall_destination_domains'] = 'Setturi destinazzjoni';
+$lang['egress_firewall_destination_ports'] = 'Destinazzjoni Port (i)';
+$lang['egress_firewall_mode'] = 'modalità';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Jimblokka traffiku kollu ħierġa - tispeċifika destinazzjonijiet permessi';
