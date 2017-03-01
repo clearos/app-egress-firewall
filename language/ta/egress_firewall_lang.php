2a60e2c69de111db3704f6d59b7494ea0d3ba706
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:51:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'அனைத்து அனுமதி';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'அனைத்து வெளிச்செல்லும் போக்குவரத்து அனுமதி - தொகுதி இடங்களுக்கு குறிப்பிட';
+$lang['egress_firewall_app_description'] = 'சாலைகளை ஃபயர்வால் பயன்பாட்டை நீங்கள் உங்கள் பிணையத்தில் இருந்து விலகுவதைத் போக்குவரத்து சில வகையான தடுக்க அனுமதிக்கிறது.';
+$lang['egress_firewall_app_name'] = 'சாலைகளை ஃபயர்வால்';
+$lang['egress_firewall_block_all'] = 'பிளாக் அனைத்து';
+$lang['egress_firewall_destination_domains'] = 'இலக்கு களங்கள்';
+$lang['egress_firewall_destination_ports'] = 'இலக்கு துறை (கள்)';
+$lang['egress_firewall_mode'] = 'முறை';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'அனைத்து வெளிச்செல்லும் போக்குவரத்து தடுக்க - அனுமதி இடங்களுக்கு குறிப்பிட';
