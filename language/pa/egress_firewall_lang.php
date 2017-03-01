2582cb29a0bf82995fee14eb0ae07ffd6525f731
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:34:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'ਸਾਰੇ ਦੀ ਇਜ਼ਾਜਤ';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'ਸਾਰੇ ਕੀਤੀ ਜਾਣ ਵਾਲੀ ਆਵਾਜਾਈ ਦੀ ਇਜ਼ਾਜਤ - ਬਲਾਕ ਨਿਸ਼ਾਨੇ ਨਿਰਧਾਰਤ ਕਰੋ';
+$lang['egress_firewall_app_description'] = 'Egress ਫਾਇਰਵਾਲ ਅਨੁਪ੍ਰਯੋਗ ਤੁਹਾਨੂੰ ਆਪਣੇ ਨੈੱਟਵਰਕ ਨੂੰ ਛੱਡ ਤੱਕ ਆਵਾਜਾਈ ਦੇ ਕੁਝ ਕਿਸਮ ਦੇ ਬਲਾਕ ਕਰਨ ਲਈ ਸਹਾਇਕ ਹੈ.';
+$lang['egress_firewall_app_name'] = 'Egress ਫਾਇਰਵਾਲ';
+$lang['egress_firewall_block_all'] = 'ਬਲਾਕ ਦੇ ਸਾਰੇ';
+$lang['egress_firewall_destination_domains'] = 'ਨਿਸ਼ਾਨਾ ਡੋਮੇਨ';
+$lang['egress_firewall_destination_ports'] = 'ਨਿਸ਼ਾਨਾ ਪੋਰ੍ਟ (ਹਵਾਈਅੱਡੇ)';
+$lang['egress_firewall_mode'] = 'ਮੋਡ';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'ਸਾਰੇ ਕੀਤੀ ਜਾਣ ਵਾਲੀ ਟ੍ਰੈਫਿਕ ਬਲਾਕ - ਦੀ ਇਜਾਜ਼ਤ ਨਿਸ਼ਾਨੇ ਨਿਰਧਾਰਤ ਕਰੋ';
