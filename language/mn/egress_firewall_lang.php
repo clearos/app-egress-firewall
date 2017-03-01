98058fe6b0aec9fca6cac37f0dbc91f2f45da042
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:26:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Бүгдийг нь зөвшөөрөх';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'бүх гарч байгаа урсгалыг зөвшөөрөх - блок очих зааж';
+$lang['egress_firewall_app_description'] = 'Хаалгатай Firewall програм нь таны сүлжээнд гарч ирсэн урсгалыг тодорхой төрлийн хаах боломжийг олгодог.';
+$lang['egress_firewall_app_name'] = 'хаалгатай Firewall';
+$lang['egress_firewall_block_all'] = 'блок мэдээлэл';
+$lang['egress_firewall_destination_domains'] = 'Destination домэйн';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (ууд)';
+$lang['egress_firewall_mode'] = 'Горим';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'бүх гарч байгаа урсгалыг хаахын - зөвшөөрсөн очих зааж';
