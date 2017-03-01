4128448215239eef1e928f0d3156c5c9ae1637f2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:19:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'leisti visiems';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Leisti visiems srautą, išeinantį - nurodyti blokuoti miestai';
+$lang['egress_firewall_app_description'] = 'Katapultowania ugniasienė programa leidžia jums blokuoti tam tikrų rūšių eismo išvykti iš savo tinklą.';
+$lang['egress_firewall_app_name'] = 'katapultowania užkarda';
+$lang['egress_firewall_block_all'] = 'blokuoti visus';
+$lang['egress_firewall_destination_domains'] = 'Paskirtis domenai';
+$lang['egress_firewall_destination_ports'] = 'Paskirties uosto (-ai)';
+$lang['egress_firewall_mode'] = 'režimas';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Blokuoti visą duomenų srautą, išeinantį - nurodykite leidžiama miestai';
