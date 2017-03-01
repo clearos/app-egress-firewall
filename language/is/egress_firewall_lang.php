bb5f16191155ed458f1dc75c50e6d2d531efc4ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:06:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'leyfa öllum';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Leyfa öllum útleið umferð - tilgreina blokk áfangastaði';
+$lang['egress_firewall_app_description'] = 'The útgangs Firewall app gerir þér kleift að loka fyrir ákveðnar tegundir af umferð frá afgangur þinn net.';
+$lang['egress_firewall_app_name'] = 'útgangs Firewall';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Destination Lén';
+$lang['egress_firewall_destination_ports'] = 'Áfangastaður Port (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Loka öllum útleið umferð - tilgreina leyfð áfangastaði';
