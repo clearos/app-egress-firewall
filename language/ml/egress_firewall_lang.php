d54e9367719903b85417adbe8fb5b7edcd129068
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:25:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'എല്ലാം അനുവദിക്കുക';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'അയയ്ക്കുന്ന എല്ലാ ട്രാഫിക് അനുവദിക്കാൻ - ബ്ലോക്ക് ലക്ഷ്യസ്ഥാനങ്ങളിലേക്കുള്ള വ്യക്തമാക്കുക';
+$lang['egress_firewall_app_description'] = 'കുറിക്കൊൾക ഫയർവാൾ ആപ്പ് നിങ്ങളുടെ നെറ്റ്വർക്ക് ഉപേക്ഷിക്കുന്നതിൽ നിന്ന് ട്രാഫിക്ക് ചില തരത്തിലുള്ള തടയാൻ അനുവദിക്കുന്നു.';
+$lang['egress_firewall_app_name'] = 'കുറിക്കൊൾക ഫയർവാൾ';
+$lang['egress_firewall_block_all'] = 'എല്ലാം തടയുക';
+$lang['egress_firewall_destination_domains'] = 'ലക്ഷ്യസ്ഥാനം ഡൊമെയ്നുകൾ';
+$lang['egress_firewall_destination_ports'] = 'ലക്ഷ്യസ്ഥാനം പോര്ട് (ങ്ങൾ)';
+$lang['egress_firewall_mode'] = 'മോഡ്';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'അയയ്ക്കുന്ന എല്ലാ ട്രാഫിക് തടയുക - അനുവദനീയമാണ് കേന്ദ്രങ്ങളിലൊന്നാണ് വ്യക്തമാക്കാൻ';
