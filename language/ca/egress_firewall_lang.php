c337f3aa980f84f0adf1db8dc6bb5381b4e86dfb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:39:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'permetre tot';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Permetre tot el trànsit de sortida - especificar destinataris de bloc';
+$lang['egress_firewall_app_description'] = 'L&#39;aplicació de graduació Firewall li permet bloquejar certs tipus de trànsit surtin de la xarxa.';
+$lang['egress_firewall_app_name'] = 'Firewall de graduació';
+$lang['egress_firewall_block_all'] = 'bloquejar tot';
+$lang['egress_firewall_destination_domains'] = 'Dominis de destinació';
+$lang['egress_firewall_destination_ports'] = 'Port de destinació (s)';
+$lang['egress_firewall_mode'] = 'manera';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bloquejar tot el tràfic sortint - especificar destinataris permesos';
