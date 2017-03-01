2295441c0519daadee9dac93ea812555f437aa9f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:54:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'อนุญาตทั้งหมด';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'ให้การจราจรขาออกทั้งหมด - ระบุจุดหมายปลายทางบล็อก';
+$lang['egress_firewall_app_description'] = 'แอปพลิเค Egress Firewall ช่วยให้คุณบล็อกบางชนิดของการจราจรออกจากเครือข่ายของคุณ';
+$lang['egress_firewall_app_name'] = 'Firewall ออกไปข้างนอก';
+$lang['egress_firewall_block_all'] = 'บล็อกทั้งหมด';
+$lang['egress_firewall_destination_domains'] = 'โดเมนปลายทาง';
+$lang['egress_firewall_destination_ports'] = 'พอร์ตปลายทาง (s)';
+$lang['egress_firewall_mode'] = 'โหมด';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'ปิดกั้นการจราจรขาออกทั้งหมด - ระบุสถานที่ท่องเที่ยวที่ได้รับอนุญาต';
