b67a12ac55ca5d984c78f14a48658ac89f6c1f10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:16:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Huc omnes';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Sine omni exitu et in negotiatione - scandalum speciem destinations';
+$lang['egress_firewall_app_description'] = 'Egressus Firewall app sino vos angustos quidam genera relicto network traffic de.';
+$lang['egress_firewall_app_name'] = 'Egress Firewall';
+$lang['egress_firewall_block_all'] = 'Acta omnia';
+$lang['egress_firewall_destination_domains'] = 'Destination Domains';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Modus';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Clausus totus traffic exitu - determinare liceat destinations';
