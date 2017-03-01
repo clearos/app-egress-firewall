62798ba3560d8b02c9f6b081acab2815bf6c73ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:40:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'amalola onse';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Kulola magalimoto onse kufalitsa - mwachindunji chipika Malo';
+$lang['egress_firewall_app_description'] = 'The app Egress makhoma oteteza amalola kuti ndigwire mitundu ina ya magalimoto anasiya maukonde anu.';
+$lang['egress_firewall_app_name'] = 'Egress makhoma oteteza';
+$lang['egress_firewall_block_all'] = 'Block onse';
+$lang['egress_firewall_destination_domains'] = 'kopita madambwe';
+$lang['egress_firewall_destination_ports'] = 'Kopita Port (s)';
+$lang['egress_firewall_mode'] = 'akafuna';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Ndigwire magalimoto onse kufalitsa - mwachindunji Malo analola';
