70020c98950244ced40e478f24e7e812b305bb50
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 11:25:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Lejo All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Lejoni të gjithë trafikun që po largohet - specifikoni destinacionet bllok';
+$lang['egress_firewall_app_description'] = 'App Daljes Firewall ju lejon për të bllokuar llojeve të caktuara të trafikut të largohet nga rrjetit tuaj.';
+$lang['egress_firewall_app_name'] = 'Firewall dalje';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'Fushat Destinacioni';
+$lang['egress_firewall_destination_ports'] = 'Destinacioni Port (s)';
+$lang['egress_firewall_mode'] = 'mënyrë';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Bllokuar të gjithë trafikun që po largohet - specifikoni destinacionet lejuara';
