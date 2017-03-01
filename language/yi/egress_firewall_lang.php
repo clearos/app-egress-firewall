6cc0b801bcb310c46445631b6a17ee422c01ad7e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:58:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'לאָזן אַלע';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'לאָזן אַלע אַוטגאָוינג פאַרקער - ספּעציפיצירן בלאָק דעסטאַניישאַנז';
+$lang['egress_firewall_app_description'] = 'די עגרעסס Firewall אַפּ אַלאַוז איר צו פאַרשפּאַרן זיכער מינים פון פאַרקער פון געלאזן אייער נעץ.';
+$lang['egress_firewall_app_name'] = 'עגרעסס Firewall';
+$lang['egress_firewall_block_all'] = 'בלאקירונג אַלע';
+$lang['egress_firewall_destination_domains'] = 'דעסטינאַטיאָן דאָומיינז';
+$lang['egress_firewall_destination_ports'] = 'דעסטינאַטיאָן פּאָרט (s)';
+$lang['egress_firewall_mode'] = 'מאָדע';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'בלאקירונג אַלע אַוטגאָוינג פאַרקער - ספּעציפיצירן ערלויבט דעסטאַניישאַנז';
