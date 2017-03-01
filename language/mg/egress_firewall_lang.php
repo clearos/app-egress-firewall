81c91f56c4fc4104b9989a62b5de363867494356
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:21:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'Avelao rehetra';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Avelao sariaka rehetra fifamoivoizana - mamaritra andian-tsoratra toerana';
+$lang['egress_firewall_app_description'] = 'Ny fivoahana Firewall fampiharana mamela anao hanakatona karazana fifamoivoizana sasany tsy hivoaka ny tambajotra.';
+$lang['egress_firewall_app_name'] = 'fivoahana Firewall';
+$lang['egress_firewall_block_all'] = 'Block rehetra';
+$lang['egress_firewall_destination_domains'] = 'Destination Domains';
+$lang['egress_firewall_destination_ports'] = 'Destination Port (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Hanakana rehetra sariaka fifamoivoizana - mamaritra namela toerana';
