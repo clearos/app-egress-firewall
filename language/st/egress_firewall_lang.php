25c7015401f747436eb0e1fd6f7c4e9929ab1096
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:46:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'lumella All';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'Lumella sephethephethe tsohle tse tswang - totobatsa bolokong celow';
+$lang['egress_firewall_app_description'] = 'The app e Egress Pula Tsa Lehlohonolo lumella u ho thibela mefuta e itseng ea sephethephethe tsoang siea ya netweke ya hao.';
+$lang['egress_firewall_app_name'] = 'Egress Pula Tsa Lehlohonolo';
+$lang['egress_firewall_block_all'] = 'Block All';
+$lang['egress_firewall_destination_domains'] = 'eang teng domains';
+$lang['egress_firewall_destination_ports'] = 'Eang teng Port (s)';
+$lang['egress_firewall_mode'] = 'Mode';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'Thibela sephethephethe tsohle tse tswang - totobatsa celow lumelloa';
