3215f3d4deeab77d4f96b7b8a1157dc85d362f18
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:00:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'सभी को अनुमति दें';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'सब बाहर जाने वाले यातायात की अनुमति दें - ब्लॉक स्थलों निर्दिष्ट';
+$lang['egress_firewall_app_description'] = 'निकास फ़ायरवॉल app आप अपने नेटवर्क छोड़ने से यातायात के कुछ प्रकार के ब्लॉक करने के लिए अनुमति देता है।';
+$lang['egress_firewall_app_name'] = 'निकास फ़ायरवॉल';
+$lang['egress_firewall_block_all'] = 'सभी को अवरोधित करें';
+$lang['egress_firewall_destination_domains'] = 'गंतव्य डोमेन';
+$lang['egress_firewall_destination_ports'] = 'गंतव्य पोर्ट (s)';
+$lang['egress_firewall_mode'] = 'मोड';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'सभी निवर्तमान यातायात अवरुद्ध - अनुमति स्थलों निर्दिष्ट';
