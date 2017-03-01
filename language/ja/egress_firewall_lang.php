3a56c94cbc9bbb4f12c2bf51f695863d82924770
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 12:09:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/egress_firewall_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['egress_firewall_allow_all'] = 'すべて許可';
+$lang['egress_firewall_allow_all_and_specify_block_destinations'] = 'すべての発信トラフィックを許可 - ブロック先を指定します';
+$lang['egress_firewall_app_description'] = '退出ファイアウォールアプリは、あなたのネットワークを残してから特定の種類のトラフィックをブロックすることができます。';
+$lang['egress_firewall_app_name'] = '出口ファイアウォール';
+$lang['egress_firewall_block_all'] = 'すべてブロック';
+$lang['egress_firewall_destination_domains'] = '宛先ドメイン';
+$lang['egress_firewall_destination_ports'] = '宛先ポート（複数可）';
+$lang['egress_firewall_mode'] = 'モード';
+$lang['egress_firewall_web_lang_block_all_and_specify_allow_destinations'] = 'すべての発信トラフィックをブロック - 許可された宛先を指定';
